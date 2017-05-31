66fd0a58bc73d9ac92135ab947f7cc9a0e4737de
risj777 <irisjka.salome@student.hu.nl>
2017-05-31 21:19:31 +0200

    Een beetje van dit en dat in de main.
    Ik ga dit waarschijnlijk toch nooit naar de master pushen. Oh wel

--- main.c
+++ main.c
@@ -15,6 +15,42 @@
 #include "Draw_request.h"
 #include "apllication.h"
 
+void put_usage()
+{
+	UART_puts("*****USAGE GUIDE*****\r\n"
+		 "This program translates user draw request to draw function which drive the VGA display.\r\n"
+		 "The user has a certain selection of commands which can be inserted, these commands are:\r\n"
+		 "write text: this command puts text on position x/y, the font, size, text color and background color can be chosen\r\n "
+		 "		example: tekst,[xposition],[yposition],[sentence],[text color],[font name],[font type],[font size],[background color]\r\n"
+		 "		argument ranges: \r\n"
+		 "		xposition: 0 -> 320\r\n"
+		 "		yposition: 0 -> 240\r\n"
+		 "		sentence : depends on font size\r\n"
+		 "		text color: 0->256(all posible colors) or zwart,blauw,lichtblauw,groen,lichtgroen,cyaan,lichtcyaan,rood,lichtrood,magenta,lichtmagenta,bruin,geel,grijs,wit"
+		 "		font name(insert the exact representation): Times New Roman, Comic sans"
+		 "		font type(insert the exact representation): Bold, regular"
+		 "		font size: 8, 12 & 20"
+		 "		background color: same as text color"
+		 "Draw line: this command draws a line on position x/y, the font, size, text color and background color can be chosen\r\n "
+		 "		example: tekst,[xposition],[yposition],[xposition2],[yposition2],[Line thickness], [color]\r\n"
+		 "		argument ranges: \r\n"
+		 "		xposition: 0 -> 320"
+		 "		yposition: 0 -> 240"
+		 "		xposition2: 0 -> 320"
+		 "		yposition2: 0 -> 240"
+		 "		width : 0 -> 30(thicker than this will ruin the figure"
+		 "		color: 0->256(all posible colors) or zwart,blauw,lichtblauw,groen,lichtgroen,cyaan,lichtcyaan,rood,lichtrood,magenta,lichtmagenta,bruin,geel,grijs,wit"
+		 "Draw ellipse: this command draws a ellipe on position x/y with a horizontal radius r1 and vertical radius r2. "
+		 "		Wheter the ellipse is filled or not is determined by fill, thickness is determined bij thickness\r\n "
+		 "		example: tekst,[xposition],[yposition],[xposition2],[yposition2],[Line thickness], [color]\r\n"
+		 "		argument ranges: \r\n"
+		 "		xposition: 0 -> 320"
+		 "		yposition: 0 -> 240"
+		 "		xposition2: 0 -> 320"
+		 "		yposition2: 0 -> 240"
+		 "		width : 0 -> 30(thicker than this will ruin the figure"
+		 "		color: 0->256(all posible colors) or zwart,blauw,lichtblauw,groen,lichtgroen,cyaan,lichtcyaan,rood,lichtrood,magenta,lichtmagenta,bruin,geel,grijs,wit");
+}
 
 int main()
 {
@@ -24,16 +60,10 @@ int main()
 	TIMER3_Initialize();
 	UART_init();
 
-	Bitmap_to_VGA(0,0,5);
-  while(1)
-  {
-//	  UART_puts("hello world");
-//	  UART_putchar(c);
-//	  Draw_Ellipse(80, 120, 50, 50, 0, 2, VGA_COL_BLUE);
-//	  wait_msec(1000);
-//	  Draw_Ellipse(240, 120, 70, 30, 1, 0, VGA_COL_RED);
-//	  wait_msec(5000);
-//	  Clear_screen(VGA_COL_BLACK);
-  }
+	UART_puts("love\r");
+	while(1)
+	{
+
+	}
 }
 
